b35d37e15be125d2d80e08f1687f657a43bab9ea
Riansyah <muh-riansyah@mail.unpas.ac.id>
2015-04-18 08:10:48 +0700

    penambahan kelas main untuk menjalankan program

---
+++ src/main/java/barokah/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barokah;
+
+/**
+ *
+ * @author SB 604-16
+ */
+public class Main {
+    public static void main(String[] args) {
+        SoalSD sd = new SoalSD();
+        System.out.println("hasil penjumlahan  11 X 10 = "+sd.perkalian(10, 11));
+    }
+}
