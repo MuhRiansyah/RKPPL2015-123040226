8e17e93dfd2dc20a14162b1f74bef6daa9529b88
Riansyah <muh-riansyah@mail.unpas.ac.id>
2015-04-18 08:09:02 +0700

    penambahan kelas Soal SD yang berisi method penjumlahan

---
+++ src/main/java/barokah/SoalSD.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barokah;
+
+/**
+ *
+ * @author SB 604-16
+ */
+public class SoalSD extends Soal{
+   //Melakukan penghitungan penjumlahan dengan memasukan paramater terlebih dahulu
+    public int penjumlahan(int bilA, int bilB) {
+        return bilA+bilB;
+    }
+        
+}
